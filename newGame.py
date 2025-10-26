bc2901d85894644953196f9b9f2fbaa3f0767ce5
Justin31241 <jc20061@gmail.com>
2025-10-26 15:50:47 -0700

    test

--- newGame.py
+++ newGame.py
@@ -1,3 +1,23 @@
 import pygame
 
-color = 100, 100, 100
+
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 1000))
+clock = pygame.time.Clock()
+running = True
+
+red = 255, 0, 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+    clock.tick(60) 
+    pygame.draw.circle(screen, red, (500, 500), 55)
+
+pygame.quit()
+
