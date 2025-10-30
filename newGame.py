e13b04cedf2142bd2a0dd1161b5687b135ec1f52
Justin31241 <jc20061@gmail.com>
2025-10-30 11:33:24 -0700

    Paddles & Ball Physics Added

--- newGame.py
+++ newGame.py
@@ -9,15 +9,74 @@ running = True
 
 red = 255, 0, 0
 
+#Physics
+speed = 20
+ball_speed = -speed / 6
+ball_direction = "Left"
+acceleration = 1.2
+maxSpeed = 10
+x = 0
+y = 0
+ball_x = 500
+ball_y = 500
+
+
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    clock.tick(60) 
+
+    if ball_direction == "Left":
+        ball_x += ball_speed
+    else:
+        ball_x -= ball_speed
+
+    if ball_direction == "Left":
+        distance = (((x - ball_x)**2) + ((y - ball_y)**2))**0.5
+        radi_sum = (20 + 10)
+        if distance <= radi_sum:
+            ball_direction = "Right"
+    else:
+        distance = ((((1000 + x) - ball_x)**2) + ((y - ball_y)**2))**0.5
+        radi_sum = (20 + 10)
+        if distance <= radi_sum:
+            ball_direction = "Left"
+
+
+    keys = pygame.key.get_pressed()
+
+    if keys != False:
+        if keys[pygame.K_UP]:
+            new_y = (y - speed)
+            if new_y < 20:
+                y = 20
+            else:
+                y -= speed
+        elif keys[pygame.K_DOWN]:
+            new_y = (y + speed)
+            if new_y > 980:
+                y = 980
+            else:
+                y += speed
+    
+
+    screen.fill((0, 0, 0))
+
+    
+    pygame.draw.circle(screen, (255, 255, 255), (x, y), 20) #Player
+    pygame.draw.circle(screen, (255, 255, 255), (1000 + x, y), 20) #Opponent
+    pygame.draw.circle(screen, red, (ball_x, ball_y), 10) #Ball
+
+
+
     pygame.display.flip()
 
-    clock.tick(60) 
-    pygame.draw.circle(screen, red, (500, 500), 55)
+    
+
+    
 
 pygame.quit()
 
